5037066afe4dd344e7303f6185842ad8dfdada0d
oddlalala <38171773+oddlalala@users.noreply.github.com>
2019-06-20 16:41:59 +0800

    Update index.js

--- weijinsuo/js/index.js
+++ weijinsuo/js/index.js
@@ -21,4 +21,27 @@ $(function(){
 	$(window).on('resize',function(){
 		render();
 	}).trigger('resize');
+
+	var $wjs_banner = $('.wjs_banner');
+	var startPos = 0;
+	var length = 0;
+	var $carousel = $('.carousel');
+	var isMove = false;
+	$wjs_banner.on('touchstart',function(e){
+		startPos = e.originalEvent.targetTouches[0].clientX;
+	}).on('touchmove',function(e){
+		length = e.originalEvent.targetTouches[0].clientX - startPos;
+		isMove = true;
+	}).on('touchend',function(e){
+		console.log(length);
+		if (length < -50) {
+			$carousel.carousel('next');
+		}
+		if (length > 50) {
+			$carousel.carousel('prev');
+		}
+		startPos = 0;
+		length = 0;
+		isMove = false;
+	});
 })
